143d092d4ad547c7cc3538acff611d320ad457a1
hrichard <hugo.richard@inria.fr>
2021-06-07 14:38:34 +0200

    add tests

---
+++ test/test_shica.py
@@ -0,0 +1,19 @@
+import numpy as np
+from shica import shica_ml, shica_j
+from shica.exp_utils import amari_d
+import matplotlib.pyplot as plt
+
+m, p, n = 4, 10, 1000
+S = np.random.randn(p, n)
+A = np.random.randn(m, p, p)
+N = np.random.randn(m, p, n)
+powers = np.random.rand(m, p)
+X = np.array([a.dot(S + p[:, None] * n) for p, a, n in zip(powers, A, N)])
+
+def test_shicaj():
+    W_pred, Sigmas, S = shica_j(X, n_components=5, verbose=True, use_jointdiag=True, use_scaling=False)
+    for w_pred, a in zip(W_pred, A):
+        # w_pred = np.linalg.inv(a)[:5, :]
+        plt.imshow(w_pred.dot(a))
+        plt.show()
+        print(np.max(np.abs(w_pred.dot(a)), axis=0))
